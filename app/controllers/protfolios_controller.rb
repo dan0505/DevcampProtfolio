cfc75691ac0ee741d1a3adf6a0eb4432ea827eb7
danielq <qiaoer0505@gmail.com>
2020-01-09 20:41:40 +1100

    add create protfolio form

--- app/controllers/protfolios_controller.rb
+++ app/controllers/protfolios_controller.rb
@@ -1,5 +1,31 @@
 class ProtfoliosController < ApplicationController
+  before_action :load_item, only: [:update]
+
   def index
     @protfolios = Protfolio.all
+  end
+
+  def new
+    @protfolio = Protfolio.new
+  end
+
+  def create
+    @protfolio = Protfolio.new(params.require(:protfolio).permit(:title, :subtitle, :body, :thumb_image))
+
+    if @protfolio.save!
+      redirect_to protfolios_path, flash: { notice: "#{@protfolio.title} saved!" }
+    else
+      redirect_to protfolios_path, flash: { error: " not saved!" }
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def load_item
+    @protfolio = Protfolio.find(params(:id))
   end
 end
