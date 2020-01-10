35d857372c36db1fe54494db7e988364727d19c2
danielq <qiaoer0505@gmail.com>
2020-01-11 10:43:17 +1100

    customized protfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :protfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  resources :protfolios, except: [:show]
+  get "protfolio/:id", to: "protfolios#show", as: "portfolio_show"
+
+  get "about-me", to: "pages#about"
+  get "contact", to: "pages#contact"
+
   resources :blogs
+  root to: "protfolios#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
